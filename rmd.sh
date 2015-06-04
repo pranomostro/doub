ee92a467e2a3f485e6cf6b62cbfb64b5cc614de7
adrian <a.regenfuss@gmx.de>
2015-06-04 23:05:16 +0200

    Cleaned the code.

--- rmd.sh
+++ rmd.sh
@@ -1,92 +1,48 @@
 #!/bin/bash
 
+until [ -e "$con" -a -e "$dis" -a -e "$sum" -a -e "$srt" ]; do
+	con=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
+	dis=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
+	sum=`echo $RANDOm | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
+	srt=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
+	touch $con $dis $sum $srt
+done
+
 if [ $# -ge 1 ]; then
 	rec=`echo $* | grep -o 'r'`
-	ver=`echo $* | grep -o 'v'`
 fi
-
-if [ -e ~/.content.log -o -e ~/.distent.log -o -e ~/.checksums.log -o -e ~/.sorted.log ]; then
-	echo "error: temporary files still exist..." >/dev/stderr
-	if [ "$ver" = 'v' ]; then
-		echo "Is another instance of this script still running or was the last one not terminated well?"
-		echo "For running this script, delete ~/.content.log, ~/.checksums.log, ~/.sorted.log and ~/.distent.log and restart this script."
-	fi
-	exit 1
-fi
-
-touch ~/.content.log ~/.distent.log ~/.checksums.log ~/.sorted.log
 
 if [ "$rec" = 'r' ]; then
-	tree -f -i --noreport | grep "[^~.]$" >~/.distent.log
+	tree -f -i --noreport | grep "[^~.]$" >$dis
 else
-	ls | grep "[^~.]$" >~/.distent.log
+	ls | grep "[^~.]$" >$dis
 fi
 
-if [ "$ver" = 'v' ]; then
-	echo "Starting to check subfolders"
-fi
-
-for ((a=`wc -l < ~/.distent.log`,b=1;b<=a;b++)); do
-	c=`sed -n "$b p" ~/.distent.log`
+for ((a=`wc -l < $dis`,b=1;b<=a;b++)); do
+	c=`sed -n "$b p" $dis`
 
 	if [ -f "$c" ]; then		
-		echo "$c" >>~/.content.log
+		echo "$c" >>$con
 	fi
 done
 
-if [ "$ver" = 'v' ]; then
-	echo "Generating checksums"
-fi
-
-for ((a=`wc -l < ~/.content.log`,b=1;b<=a;b++)); do
-	c=`sed -n "$b p" ~/.content.log`
-	cat "$c" | md5sum | sed "s/ -//" >> ~/.checksums.log
+for ((a=`wc -l < $con`,b=1;b<=a;b++)); do
+	c=`sed -n "$b p" $con`
+	cat "$c" | md5sum | sed "s/ -//" >> $sum
 done
 
-paste ~/.checksums.log ~/.content.log >~/.distent.log
-sort ~/.distent.log >~/.sorted.log
+paste $sum $con >$dis
+sort $dis >$srt
 
-
-if [ "$ver" = 'v' ]; then
-	cat ~/.content.log
-	echo "Press a key to continue"
-	read
-
-	echo ""
-	echo "Checking the sums (and if they match, the files)"
-	echo "Warning, could be slow..."
-
-	cat<<\EOF
-The Unix philosophy tells me that I should give you somthing to read while you are waiting
-for the result of this so unbearably slow program.
-Don't be angry. The first version took 23 hours for around 40 gigabyte, and that one used cmp.
-I tried to make it faster.
-And the bash is really slow...when you are used to C, like I am. But what can I do?
-I would like to just show you the checksums of your files, That would show you the progress.
-Yeah, somthing like [checksum], number [number1] of [total] would be great. Okay, let's do this!
-
-And by the way, if you want to tell me that I suck and my programs are slow, just mail me (a.regenfuss@gmx.de).
-
-I will try to reply before the earth falls into the sun.
-
-EOF
-fi
-for ((a=`wc -l < ~/.checksums.log`,b=1,c=2;c<=a;)); do
-	d=`sed -n "$b p" ~/.sorted.log | awk -e '{ print $1 }'`
-	e=`sed -n "$c p" ~/.sorted.log | awk -e '{ print $1 }'`
-
-	if [ "$ver" = 'v' ]; then
-		echo "$d, number $b of $a"
-	fi
+for ((a=`wc -l < $sum`,b=1,c=2;c<=a;)); do
+	d=`sed -n "$b p" $srt | awk -e '{ print $1 }'`
+	e=`sed -n "$c p" $srt | awk -e '{ print $1 }'`
 
 	if [ $d == $e ]; then
-		f=`cat ~/.sorted.log | sed -n "${b}s/[0-9a-f]\+ \t//p"`
-		g=`cat ~/.sorted.log | sed -n "${c}s/[0-9a-f]\+ \t//p"`
+		f=`cat $srt | sed -n "${b}s/[0-9a-f]\+ \t//p"`
+		g=`cat $srt | sed -n "${c}s/[0-9a-f]\+ \t//p"`
 		cmp -s "$f" "$g"
 		if(($?==0));then
-			if [ "$ver" == 'v' ]; then
-				echo "Deleting $g, keeping $f."
-			fi
 			rm $g
 		fi
 		let "c=$c+1"
@@ -96,6 +52,6 @@ for ((a=`wc -l < ~/.checksums.log`,b=1,c=2;c<=a;)); do
 	fi
 done
 
-rm ~/.distent.log ~/.content.log  ~/.checksums.log ~/.sorted.log
+rm $con $dis $sum $srt
 
 exit 0
