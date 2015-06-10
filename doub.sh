a46927e6875f61d3da800e4f69fdb55d87137596
adrian <a.regenfuss@gmx.de>
2015-06-11 00:00:53 +0200

    Printing files is so much nicer than deleting them, isn't it?

--- doub.sh
+++ doub.sh
@@ -19,7 +19,7 @@ fi
 for ((a=`wc -l < $dis`,b=1;b<=a;b++)); do
 	c=`sed -n "$b p" $dis`
 
-	if [ -f "$c" ]; then		
+	if [ -f "$c" ]; then
 		echo "$c" >>$con
 	fi
 done
@@ -40,7 +40,7 @@ for ((a=`wc -l < $sum`,b=1,c=2;c<=a;)); do
 		f=`cat $srt | sed -n "${b}s/[0-9a-f]\+ \t//p"`
 		g=`cat $srt | sed -n "${c}s/[0-9a-f]\+ \t//p"`
 
-		if cmp -s "$f" "$g"; then rm $g;fi
+		if cmp -s "$f" "$g"; then echo $g;fi
 
 		let "c=$c+1"
 	else
