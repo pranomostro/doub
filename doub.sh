b6b812a6d104234e4366b3fd179e08864784836c
adrian <a.regenfuss@gmx.de>
2015-06-13 15:53:43 +0200

    Did more variable renaming.

--- doub.sh
+++ doub.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-until [ -e "$raw" -a -e "$fil" -a -e "$sum" -a -e "$srt" ]; do
-	con=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
-	dis=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
+until [ -e "$fil" -a -e "$raw" -a -e "$sum" -a -e "$srt" ]; do
+	fil=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
+	raw=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
 	sum=`echo $RANDOm | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
 	srt=`echo $RANDOM | md5sum | sed 's/ .*//' | sed 's/.*/\/tmp\/&/'`
-	touch $raw $fil $sum $srt
+	touch $fil $raw $sum $srt
 done
 
 dir="$1"; rec="$2"
 
 if [ "$rec" = '-r' ]; then
-	du -a $dir | sed 's/^[0-9]\+\t//' | sed '$d' >$fil
+	du -a $dir | sed 's/^[0-9]\+\t//' | sed '$d' >$raw
 else
-	ls $dir >$fil
+	ls $dir >$raw
 fi
-
-for ((a=`wc -l < $fil`,b=1;b<=a;b++)); do
-	c=`sed -n "$b p" $fil`
-
-	if [ -f "$c" ]; then
-		echo "$c" >>$raw
-	fi
-done
 
 for ((a=`wc -l < $raw`,b=1;b<=a;b++)); do
 	c=`sed -n "$b p" $raw`
+
+	if [ -f "$c" ]; then
+		echo "$c" >>$fil
+	fi
+done
+
+for ((a=`wc -l < $fil`,b=1;b<=a;b++)); do
+	c=`sed -n "$b p" $fil`
 	cat "$c" | md5sum | sed "s/ -//" >> $sum
 done
 
-paste $sum $raw >$fil
-sort $fil >$srt
+paste $sum $fil >$raw
+sort $raw >$srt
 
-for ((a=`wc -l < $srt`,b=1,c=2;c<=a;)); do
+for ((a=`wc -l < $sum`,b=1,c=2;c<=a;)); do
 	d=`sed -n "$b p" $srt | awk -e '{ print $1 }'`
 	e=`sed -n "$c p" $srt | awk -e '{ print $1 }'`
 
@@ -49,6 +49,6 @@ for ((a=`wc -l < $srt`,b=1,c=2;c<=a;)); do
 	fi
 done
 
-rm $raw $fil $sum $srt
+rm $fil $raw $sum $srt
 
 exit 0
