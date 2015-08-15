14d5cbc3a1cb349502c999ee4a1a6f94d901f6ce
scharlatan <a.regenfuss@gmx.de>
2015-08-16 00:11:05 +0200

    Enclosed variables in quotes.

--- doub.sh
+++ doub.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eo pipefail
+set -eruo pipefail
 
 #Since bash doesn't know do { } while (); we have to do this.
 
@@ -10,38 +10,38 @@ while [ -e "$fil" -o -e "$raw" -o -e "$sum" -o -e "$srt" ]; do
 	fil=`echo /tmp/$RANDOM`.$$; raw=`echo /tmp/$RANDOM`.$$; sum=`echo /tmp/$RANDOM`.$$; srt=`echo /tmp/$RANDOM`.$$
 done
 
-touch $fil $raw $sum $srt
+touch "$fil" "$raw" "$sum" "$srt"
 
 dir="$1"; rec="$2"
 
 if [ "$rec" = '-r' ]; then
-	du -a $dir | sed 's/^[0-9]\+\t//' | sed '$d' >$raw
+	du -a $dir | sed 's/^[0-9]\+\t//' | sed '$d' >"$raw"
 else
-	ls $dir >$raw
+	ls "$dir" >"$raw"
 fi
 
-for ((a=`wc -l < $raw`,b=1;b<=a;b++)); do
-	c=`sed -n "$b p" $raw`
+for ((a=`wc -l < "$raw"`,b=1;b<=a;b++)); do
+	c=`sed -n "$b p" "$raw"`
 
 	if [ -f "$c" ]; then
-		echo "$c" >>$fil
+		echo "$c" >>"$fil"
 	fi
 done
 
 for ((a=`wc -l < $fil`,b=1;b<=a;b++)); do
-	c=`sed -n "$b p" $fil`
-	cat "$c" | md5sum | sed "s/ -//" >> $sum
+	c=`sed -n "$b p" "$fil"`
+	cat "$c" | md5sum | sed "s/ -//" >> "$sum"
 done
 
-paste $sum $fil >$raw
-sort $raw >$srt
+paste "$sum" "$fil" >"$raw"
+sort "$raw" >"$srt"
 
 awk '{
 	if (lastsum==$1) { printf("%s ",lastname); lasteq=1 }
 	if (lastsum!=$1&&lasteq==1) { printf("%s\n", lastname); lasteq=0 }
 	lastsum=$1; lastname=$2
-}' $srt
+}' "$srt"
 
-rm $fil $raw $sum $srt
+rm "$fil" "$raw" "$sum" "$srt"
 
 exit 0
